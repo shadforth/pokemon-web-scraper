7b9a64b340a5a8f3117bc758c9b3995c1044d1ad
shadforth <s3332777@student.rmit.edu.au>
2018-12-04 14:51:14 +1100

    Add logic to handle output to JSON, add logging, and sample output file

--- pokemon_web_scraper.py
+++ pokemon_web_scraper.py
@@ -2,47 +2,69 @@
 """
 This module scrapes Serebii.net for a given Pokémon.
 """
+import json
+import logging
+import pprint
 import requests
 import bs4
 
 from colorama import Fore, Back, Style
 
-NUMBER_LENGTH = 3
+logging.basicConfig(level=logging.DEBUG)
 
-def get_pokemon_data(limit=10):
+OUTPUT_FILE_NAME = 'pokemon.json'
+
+def get_pokemon_data(limit=10, output_json=False):
     """
     Scrape Serebii.net for Pokémon data and output to console.
     :param limit: The maximum pokemon number to retrieve information from.
     """
 
-    for i in range(1, limit):
+    if output_json:
+        output_file = open(OUTPUT_FILE_NAME, 'w+')
+        all_pokemon = []
+
+    for i in range(1, (limit + 1)):
         # Extract data from Serebii.net
-        url = 'https://serebii.net/pokedex/{}.shtml'.format(str(i).zfill(NUMBER_LENGTH))
+        logging.debug('Extracting data from Serebii.net')
+        url = 'https://serebii.net/pokedex/{}.shtml'.format(str(i).zfill(3))
         data = requests.get(url)
         soup = bs4.BeautifulSoup(data.text, 'html.parser')
-        all_divs = soup.find_all('div', attrs={'align': 'center'})
-        center_panel_info = all_divs[3].findAll('td', {'class': 'fooinfo'})
+        try: 
+            all_divs = soup.find_all('div', attrs={'align': 'center'})
+            center_panel_info = all_divs[3].findAll('td', {'class': 'fooinfo'})
+        except Exception as e:
+            logging.error('There was an error trying to identify elements on the webpage.')
 
         pokemon = {}
         pokemon['name']           = center_panel_info[1].text
         pokemon['number']         = center_panel_info[3].text
         pokemon['classification'] = center_panel_info[4].text
         pokemon['height']         = (center_panel_info[5].text).replace('\r', '').replace('\n', '').replace('\t\t\t', ',').split(',')
-        pokemon['weight']        = (center_panel_info[6].text).replace('\r', '').replace('\n', '').replace('\t\t\t', ',').split(',')
+        pokemon['weight']         = (center_panel_info[6].text).replace('\r', '').replace('\n', '').replace('\t\t\t', ',').split(',')
         
         # Get data from stats table
         all_tables = soup.find_all('table', attrs={'class': 'dextable'})
         base_stats_table = all_tables[7].findAll('td')
 
-        pokemon['hit_points'] = base_stats_table[8].text
-        pokemon['attack']     = base_stats_table[9].text
-        pokemon['defense']    = base_stats_table[10].text
-        pokemon['special']    = base_stats_table[11].text
-        pokemon['speed']      = base_stats_table[12].text
+        pokemon['hit_points'] = int(base_stats_table[8].text)
+        pokemon['attack']     = int(base_stats_table[9].text)
+        pokemon['defense']    = int(base_stats_table[10].text)
+        pokemon['special']    = int(base_stats_table[11].text)
+        pokemon['speed']      = int(base_stats_table[12].text)
 
         # Print the data out nicely
+        print('=' * 30)
         print_pokemon_data(pokemon)
-        print('-' * 30)
+        print('=' * 30)
+
+        if output_json:
+            all_pokemon.append(pokemon)
+    
+    if output_json:
+        logging.info('Writing to {}'.format(OUTPUT_FILE_NAME))
+        output_file.write(json.dumps(all_pokemon))
+        output_file.close()
 
 
 def print_pokemon_data(pokemon):
@@ -63,4 +85,4 @@ def print_pokemon_data(pokemon):
 
 
 if __name__ == '__main__':
-    get_pokemon_data()
+    get_pokemon_data(3, True)
